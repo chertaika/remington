d6d6fc77a88012855cd338fa9450dcd0cc9f7b61
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-11-27 22:52:53 +0700

    feat: реализовать блок Shop

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import './Main.css';
 import Companies from '../Companies/Companies';
 import Subscribe from '../Subscribe/Subscribe';
 import ContentContext from '../../contexts/ContentContext';
+import Shop from '../Shop/Shop';
 
 const Main = ({ slides }) => {
   const content = useContext(ContentContext);
@@ -14,6 +15,28 @@ const Main = ({ slides }) => {
       {content.filter(item => item.catalog).length > 1
         && (<Companies />)}
       <Subscribe />
+      {content.map(({
+        catalog,
+        description,
+        vk_link: vkLink,
+        slug,
+        title,
+        shop_id: shopId,
+      }) => {
+        if (catalog) {
+          return (
+            <Shop
+              slug={slug}
+              title={title}
+              description={description}
+              vkLink={vkLink}
+              catalog={catalog}
+              key={shopId}
+            />
+          );
+        }
+        return false;
+      })}
     </main>
   );
 };
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,29 @@
+import './Shop.css';
+import { Link } from 'react-router-dom';
+
+const Shop = ({
+  title,
+  description,
+  vkLink,
+  slug,
+}) => (
+  <section className="shop" id={slug}>
+    <div className="shop__description">
+      <h2 className="shop__title">{title}</h2>
+      <div className="shop__main-text">
+        <div className="shop__line line" />
+        <p className="shop__paragraph">
+          {description}
+        </p>
+        {vkLink && (
+          <Link className="shop__social-link button-hover" to={vkLink} target="_blank">
+            <span className="shop__link-text" />
+            <span className="shop__social-icon social-icon" />
+          </Link>
+        )}
+      </div>
+    </div>
+  </section>
+);
+
+export default Shop;
